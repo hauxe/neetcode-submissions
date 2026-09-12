2847015490ad53202264acbee33323ffbd2c83ec
Kelvin Ma <16794843+hauxe@users.noreply.github.com>
2026-09-12 09:16:06 +0700

    Add: longest-increasing-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-0.go
@@ -0,0 +1,16 @@
+func lengthOfLIS(nums []int) int {
+	dp := make([]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		dp[i] = 1
+	}
+	maxVal := 1
+	for i := 1; i < len(nums); i++ {
+		for j := 0; j < i; j++ {
+			if nums[j] < nums[i] {
+				dp[i] = max(dp[i], dp[j]+1)
+			}
+		}
+		maxVal = max(maxVal, dp[i])
+	}
+	return maxVal
+}
